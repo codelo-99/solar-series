12f6b86d6bb75d176fac73a3e5670deb23666dbe
codelo <759614364@qq.com>
2024-03-28 21:36:06 +0800

    feat: 6.1 add preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
-import {defineConfig} from 'astro/config';
+import { defineConfig } from 'astro/config';
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://main--solar-series.netlify.app'
+  site: 'https://main--solar-series.netlify.app',
+  integrations: [preact()]
 });
